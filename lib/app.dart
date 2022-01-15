478df1189edd7ebcc80381612a359e80e66859bf
Đỗ Quang Trung <dotrunghg1999@gmail.com>
2022-01-15 16:49:56 +0700

    init and change some code

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:new_flutter_app/home/location_detail.dart';
+
+class App extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LocationDetail(),
+    );
+  }
+}
